da9bcc13eb7560bbb8f2d1c74d69dc171379d692
oscar311 <oscar.downing71@gmail.com>
2017-12-01 12:53:37 +1100

    encrypted password

--- app/Login.php
+++ app/Login.php
@@ -1,26 +1,28 @@
 <?php
+    require("password.php");
+
     $con = mysqli_connect("localhost", "id3776519_eventblockusers", "Oscar191097", "id3776519_users");
     
     $username = $_POST["username"];
     $password = $_POST["password"];
     
-    $statement = mysqli_prepare($con, "SELECT * FROM Users WHERE username = ? AND password = ?");
-    mysqli_stmt_bind_param($statement, "ss", $username, $password);
+    $statement = mysqli_prepare($con, "SELECT * FROM Users WHERE username = ?");
+    mysqli_stmt_bind_param($statement, "s", $username);
     mysqli_stmt_execute($statement);
-    
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $userID, $name, $username, $age, $password);
+    mysqli_stmt_bind_result($statement, $colUserID, $colName, $colUsername, $colAge, $colPassword);
     
     $response = array();
     $response["success"] = false;  
     
     while(mysqli_stmt_fetch($statement)){
-        $response["success"] = true;  
-        $response["name"] = $name;
-        $response["age"] = $age;
-        $response["username"] = $username;
-        $response["password"] = $password;
+        if (password_verify($password, $colPassword)) {
+            $response["success"] = true;  
+            $response["name"] = $colName;
+            $response["age"] = $colAge;
+            $response["username"] = $colUsername;
+        }
     }
-    
+
     echo json_encode($response);
 ?>
--- app/Register.php
+++ app/Register.php
@@ -1,17 +1,44 @@
 <?php
-    $con = mysqli_connect("localhost", "id3776519_eventblockusers", "Oscar191097", "id3776519_users");
+    require("password.php");
+
+    $connect = mysqli_connect("localhost", "id3776519_eventblockusers", "Oscar191097", "id3776519_users");
     
     $name = $_POST["name"];
     $age = $_POST["age"];
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $statement = mysqli_prepare($con , "INSERT INTO Users(name,username,age,password) VALUES(?,?,?,?)");
-    mysqli_stmt_bind_param($statement, "ssis", $name, $username, $age, $password);
-    mysqli_stmt_execute($statement);
-    
+     function registerUser() {
+        global $connect, $name, $age, $username, $password;
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+        $statement = mysqli_prepare($connect, "INSERT INTO Users (name, username, age, password) VALUES (?, ?, ?, ?)");
+        mysqli_stmt_bind_param($statement, "ssis", $name, $username, $age, $passwordHash);
+        mysqli_stmt_execute($statement);
+        mysqli_stmt_close($statement);     
+    }
+
+    function usernameAvailable() {
+        global $connect, $username;
+        $statement = mysqli_prepare($connect, "SELECT * FROM Users WHERE username = ?"); 
+        mysqli_stmt_bind_param($statement, "s", $username);
+        mysqli_stmt_execute($statement);
+        mysqli_stmt_store_result($statement);
+        $count = mysqli_stmt_num_rows($statement);
+        mysqli_stmt_close($statement); 
+        if ($count < 1){
+            return true; 
+        }else {
+            return false; 
+        }
+    }
+
     $response = array();
-    $response["success"] = true;  
+    $response["success"] = false;  
+
+    if (usernameAvailable()){
+        registerUser();
+        $response["success"] = true;  
+    }
     
     echo json_encode($response);
 ?>
