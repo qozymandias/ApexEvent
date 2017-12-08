3506606df5ea206495abbbd89563e243803ffffe
oscar311 <oscar.downing71@gmail.com>
2017-12-08 16:42:12 +1100

    added browser (3 tabs)

---
+++ app/src/main/java/com/eventblock/eventblock/Tab1Events.java
@@ -0,0 +1,23 @@
+package com.eventblock.eventblock;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by oscar on 8/12/17.
+ */
+
+public class Tab1Events extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1_events, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/eventblock/eventblock/Tab3Profile.java
@@ -0,0 +1,22 @@
+package com.eventblock.eventblock;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by oscar on 8/12/17.
+ */
+
+public class Tab3Profile extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3_profile, container, false);
+
+        return rootView;
+    }
+}
